1804b11b0a5f388fc91136c153eebfe4424e5a87
Meeli Sirvi <95487048+Sirvimee@users.noreply.github.com>
2025-06-25 12:50:47 +0300

    Update FlightsFromSerpApiApplication.java

--- src/main/java/com/example/FlightsFromSerpApi/FlightsFromSerpApiApplication.java
+++ src/main/java/com/example/FlightsFromSerpApi/FlightsFromSerpApiApplication.java
@@ -1,15 +1,30 @@
 package com.example.FlightsFromSerpApi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.CommandLineRunner;
+import service.FlightImportScheduler;
 
 @SpringBootApplication
-@EnableScheduling
-public class FlightsFromSerpApiApplication {
+public class FlightsFromSerpApiApplication implements CommandLineRunner {
+
+	@Autowired
+  	private FlightImportScheduler flightImportScheduler;
+
+  	@Autowired
+  	private ApplicationArguments args;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightsFromSerpApiApplication.class, args);
 	}
 
+	@Override
+  	public void run(String... args) {
+    	if (this.args.containsOption("importFlights")) {
+      flightImportScheduler.importTomorrowsFlights();
+      System.exit(0); 
+    }
+  }
 }
