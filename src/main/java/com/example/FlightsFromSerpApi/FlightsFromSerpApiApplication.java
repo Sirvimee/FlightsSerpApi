f3dbfdf4d0d39fec3372a30c2f2dea4aa3784a84
Meeli Sirvi <95487048+Sirvimee@users.noreply.github.com>
2025-06-25 14:06:27 +0300

    Update FlightsFromSerpApiApplication.java

--- src/main/java/com/example/FlightsFromSerpApi/FlightsFromSerpApiApplication.java
+++ src/main/java/com/example/FlightsFromSerpApi/FlightsFromSerpApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import service.FlightImportScheduler;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.FlightsFromSerpApi", "service"})
